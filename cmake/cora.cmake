501c832fcdadc433c2f4571236ecba922951f810
Stephan Weintritt <45856463+sweintritt@users.noreply.github.com>
2020-03-27 05:47:09 +0100

    Added sqlite and protobuf as dependency

--- cmake/cora.cmake
+++ cmake/cora.cmake
@@ -1,22 +1,40 @@
 set(cora_sources
     ${cora_source_dir}/media_player.cpp
     ${cora_source_dir}/qt_media_player.cpp
+    ${cora_source_dir}/station.cpp
+    ${cora_source_dir}/sqlite_stations_dao.cpp
 )
 
 set(cora_includes
     ${cora_source_dir}
 )
 
-add_executable(cora ${cora_source_dir}/main.cpp ${cora_sources} ${cora_includes})
-
 CoraUseQtMultimedia()
+CoraUseSQLite3()
 
-include_directories(${cora_source_dir})
+include_directories(${cora_source_dir}
+    ${SQLITE3_INCLUDE_DIR})
 
 set(cora_link_libraries
     Qt5::Core
-    Qt5::Multimedia)
+    Qt5::Multimedia
+    ${SQLITE3_LIBRARY})
 
-target_link_libraries(cora ${cora_link_libraries})
 
+if(CMAKE_COMPILER_IS_GNUCXX)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pedantic -Wextra -Wall -Wundef -Wunused -Wshadow -Wfatal-errors -Wfloat-equal -ftrapv -Wunreachable-code -Weffc++")
+
+   if (CMAKE_BUILD_TYPE MATCHES "Debug")
+      message(STATUS "Activating profiling and gdb options")
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -p -pg -ggdb3 -O0 --coverage")
+   endif()
+else()
+   message(WARNING, "Unsupported compiler ${CMAKE_CXX_COMPILER_ID}. No options set.")
+endif()
+
+set(MAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+add_executable(cora ${cora_source_dir}/main.cpp ${cora_sources} ${cora_includes})
 add_executable(cora::cora ALIAS cora)
+target_link_libraries(cora ${cora_link_libraries})
--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,5 +1,53 @@
+include(ExternalProject)
 
 macro(CoraUseQtMultimedia)
     find_package(Qt5Core)
     find_package(Qt5Multimedia)
+endmacro()
+
+set(CORA_SQLITE3_PROVIDER "module" CACHE STRING "Provider of sqlite3 library")
+set_property(CACHE CORA_SQLITE3_PROVIDER PROPERTY STRINGS "module" "package")
+
+macro(CoraUseSQLite3)
+    if ("${CORA_SQLITE3_PROVIDER}" STREQUAL "module")
+        if (NOT SQLITE3_FOUND)
+          set(SQLITE3_ROOT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party/sqlite)
+            if (EXISTS "${SQLITE3_ROOT_DIR}")
+                set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+                find_package(Threads REQUIRED)
+
+                message(INFO "--- SQLITE3_ROOT_DIR:${SQLITE3_ROOT_DIR}")
+                ExternalProject_Add(project_sqlite3
+                    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/sqlite3
+                    SOURCE_DIR ${SQLITE3_ROOT_DIR}
+                    BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/sqlite3
+                    CONFIGURE_COMMAND ${SQLITE3_ROOT_DIR}/configure
+                    BUILD_COMMAND make
+                    INSTALL_COMMAND "")
+
+                ExternalProject_Get_Property(project_sqlite3 install_dir)
+                add_library(sqlite3 STATIC IMPORTED)
+                set_property(TARGET sqlite3 PROPERTY IMPORTED_LOCATION ${install_dir}/libsqlite3.la)
+                add_dependencies(sqlite3 project_sqlite3)
+
+                set(SQLITE3_FOUND TRUE)
+                set(SQLITE3_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/sqlite3)
+                set(SQLITE3_LIBRARY sqlite3)
+                message(INFO "--- SQLITE3_INCLUDE_DIR:${SQLITE3_INCLUDE_DIR}")
+                message(INFO "--- SQLITE3_LIBRARY:${SQLITE3_LIBRARY}")
+                link_directories(${CMAKE_CURRENT_BINARY_DIR}/sqlite3 ${LINK_DIRECTORIES})
+            else()
+                message(WARNING "CORA_SQLITE3_PROVIDER is \"module\" but SQLITE3_ROOT_DIR is wrong: ${SQLITE3_ROOT_DIR}")
+            endif()
+        endif()
+    elseif ("${CORA_SQLITE3_PROVIDER}" STREQUAL "package")
+        if (NOT SQLITE3_FOUND)
+           find_package(sqlite3)
+
+           if (NOT SQLITE3_FOUND)
+              find_package(PkgConfig REQUIRED)
+              pkg_check_modules(SQLITE3 REQUIRED sqlite3)
+           endif ()
+       endif()
+    endif()
 endmacro()
