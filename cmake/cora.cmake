5bbebdf9c1a1b5836736e941e8d735f0e60497af
Stephan Weintritt <45856463+sweintritt@users.noreply.github.com>
2020-04-09 13:39:33 +0200

    Added glog as dependency

--- cmake/cora.cmake
+++ cmake/cora.cmake
@@ -1,26 +1,31 @@
 set(cora_sources
     ${cora_source_dir}/media_player.cpp
     ${cora_source_dir}/qt_media_player.cpp
-    ${cora_source_dir}/station.cpp
     ${cora_source_dir}/sqlite_stations_dao.cpp
 )
 
 CoraUseQtMultimedia()
 CoraUseSQLite3()
 CoraUseProtobuf()
+CoraUseGlog()
 
 include_directories(${cora_source_dir}
     ${SQLITE3_INCLUDE_DIR}
-    ${PROTOBUF_INCLUDE_DIR})
+    ${PROTOBUF_INCLUDE_DIR}
+    ${CMAKE_CURRENT_BINARY_DIR}
+    ${GLOG_INCLUDE_DIR})
 
 set(cora_link_libraries
     Qt5::Core
     Qt5::Multimedia
     ${SQLITE3_LIBRARY}
-    ${PROTOBUF_LIBRARY})
+    ${PROTOBUF_LIBRARY}
+    ${GLOG_LIBRARY})
 
-# TODO Generate proto files
 protobuf_generate_cpp(PROTO_SRCS PROTO_HDRS ${CMAKE_CURRENT_SOURCE_DIR}/proto/station.proto)
+message(STATUS "PROTO_HDRS: ${PROTO_HDRS}")
+message(STATUS "PROTO_SRCS: ${PROTO_SRCS}")
+
 
 if(CMAKE_COMPILER_IS_GNUCXX)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pedantic -Wextra -Wall -Wundef -Wunused -Wshadow -Wfatal-errors -Wfloat-equal -ftrapv -Wunreachable-code -Weffc++")
