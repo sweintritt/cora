513d0f1e8a761b09d89383234fee204ee2b53357
Stephan Weintrit <45856463+sweintritt@users.noreply.github.com>
2021-07-10 21:29:03 +0200

    Transaction for radio sure import

--- src/importer/radio_sure_importer.cpp
+++ src/importer/radio_sure_importer.cpp
@@ -18,31 +18,44 @@ void RadioSureImporter::import(const std::string& url, const std::shared_ptr<Sta
 
     if (file.is_open()) {
         long count = 0;
-        std::time_t start = std::time(nullptr);
-        // TODO transaction
-        while (getline(file, line)) {
-            const std::vector<std::string> values = split(line, '\t');
+        const std::time_t start = std::time(nullptr);
+        stationsDao->beginTransaction();
 
-            Station station;
-            station.setName(values[0]);
-            station.setDescription(values[1]);
-            station.setGenre(values[2]);
-            station.setCountry(values[3]);
-            station.setLanguage(values[4]);
-            station.setAddedBy(getName());
+        try {
+            while (getline(file, line)) {
+                const std::vector<std::string> values = split(line, '\t');
 
-            for (unsigned int i = 5; i < values.size(); ++i) {
-              if (values[i].compare("-")) {
-                station.addUrl(values[i]);
-              }
+                Station station;
+                station.setName(values[0]);
+                station.setDescription(values[1]);
+                station.setGenre(values[2]);
+                station.setCountry(values[3]);
+                station.setLanguage(values[4]);
+                station.setAddedBy(getName());
+
+                for (unsigned int i = 5; i < values.size(); ++i) {
+                if (values[i].compare("-")) {
+                    station.addUrl(values[i]);
+                }
+                }
+
+                stationsDao->save(station);
+                ++count;
             }
 
-            stationsDao->save(station);
-            ++count;
+            LOG(plog::info) << count << " stations imported in " << std::difftime(std::time(nullptr), start) << " s";
+            file.close();
+            stationsDao->commit();
+        } catch (const std::exception& e) {
+            stationsDao->rollback();
+            throw e;
+        } catch (const std::string& e) {
+            stationsDao->rollback();
+            throw e;
+        } catch (...) {
+            stationsDao->rollback();
+            LOG(plog::error) << "Unknown error";
         }
-
-        LOG(plog::info) << count << " stations imported in " << std::difftime(std::time(nullptr), start) << " s";
-        file.close();
     } else {
         throw std::runtime_error("Unable to open file " + url);
     }
