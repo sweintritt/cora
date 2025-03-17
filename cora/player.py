583d91009472a1f574d04d5601e5c98f3682a535
Stephan Weintritt <45856463+sweintritt@users.noreply.github.com>
2025-03-17 06:17:35 +0100

    Cleanup

--- cora/player.py
+++ cora/player.py
@@ -1,7 +1,6 @@
 import logging
 import os
-import time
-from threading import Thread
+import threading
 
 import vlc
 
@@ -13,26 +12,25 @@ class Player:
         # turn of vlc output
         os.environ["VLC_VERBOSE"] = str("-1")
         self.player = None
-        self.printTitleThread = None
+        self.play_thread = None
+        self.metadata_timer = None
         self.playing = False
-        self.playThread = None
+        self.timer = None
 
     def set_url(self, url):
         logger.debug("url:" + url)
         self.player = vlc.MediaPlayer(url)
 
     def play(self):
-        self.playThread = Thread(target = self.player.play)
-        self.playThread.start()
+        self.play_thread = threading.Thread(target = self.player.play)
+        self.play_thread.start()
         self.playing = True
-        # FIXME this blocks somehow
-        self.printTitleThread = Thread(target = self.check_metadata)
-        self.printTitleThread.start()
+        self.metadata_timer = threading.Timer(5, self.check_metadata)
+        self.metadata_timer.start()
 
     def check_metadata(self):
         previous = ""
         while self.playing:
-            time.sleep(5)
             logger.debug("checking stream metadata")
             meta = self.player.get_media().get_meta(12) # vlc.Meta 12: 'NowPlaying',
             if meta != previous:
@@ -46,5 +44,5 @@ class Player:
         if self.player is not None:
             self.player.stop()
 
-        #if self.printTitleThread is not None:
-        #    self.printTitleThread.join()
+        if self.metadata_timer is not None:
+            self.metadata_timer.cancel()
