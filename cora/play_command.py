1d3facb06dd3e9b064401fbe5676105c1d4aa25e
Stephan Weintritt <45856463+sweintritt@users.noreply.github.com>
2025-03-27 05:24:01 +0100

    Play command handles 'no station found' better

--- cora/play_command.py
+++ cora/play_command.py
@@ -27,12 +27,15 @@ class PlayCommand(command.Command):
             logger.debug("search: " + keywords)
             station = self.stations.find_by_keywords(keywords)
 
-        logger.info("playing " + station.name.strip())
-        self.player.set_url(station.urls[0])
-        self.player.play()
-        playing = True
-        while playing:
-            logger.info("Press any key to stop playing")
-            key = input()
-            playing = False
-        self.player.stop()
+        if station is None:
+            logger.info("No station found for " + str(args.search))
+        else:
+            logger.info("playing " + station.name.strip())
+            self.player.set_url(station.urls[0])
+            self.player.play()
+            playing = True
+            while playing:
+                logger.info("Press any key to stop playing")
+                key = input()
+                playing = False
+            self.player.stop()
