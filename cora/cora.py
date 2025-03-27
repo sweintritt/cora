0cc2a773a4def915fcc9e4c8cd7facdb9f6c5311
Stephan Weintritt <45856463+sweintritt@users.noreply.github.com>
2025-03-27 05:22:58 +0100

    Debug logging and version command

--- cora/cora.py
+++ cora/cora.py
@@ -2,6 +2,7 @@
 
 import argparse
 import logging
+import os
 import sys
 import traceback
 
@@ -10,29 +11,39 @@ import list_command
 import play_command
 import player
 import stations
+import version_command
 
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format="%(levelname)s - %(message)s")
-
-    logger.debug("initializing commands")
     parser = argparse.ArgumentParser(
                         prog='cora',
                         description='Play internet radio streams on your console')
+    parser.add_argument('-d', '--debug',
+                        action=argparse.BooleanOptionalAction,
+                        required=False)
     subparsers = parser.add_subparsers(required=True)
 
     player = player.Player()
     db = stations.Stations()
-    logger.debug("opening database")
-    db.open(file="test.sqlite")
+    home_dir = os.path.expanduser('~')
+    file = home_dir + '/.cora2.sqlite'
+    print(file)
+    db.open(file=file)
 
     import_command = import_command.ImportCommand(db, None, player, subparsers)
     play_command = play_command.PlayCommand(db, None, player, subparsers)
     list_command = list_command.ListCommand(db, None, player, subparsers)
+    version_command = version_command.VersionCommand(db, None, player, subparsers)
 
     try:
         args = parser.parse_args(sys.argv[1:])
+
+        if args.debug:
+            logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format="%(levelname)s - %(message)s")
+        else:
+            logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")
+
         args.func(args)
         player.stop()
         db.close()
---
+++ cora/version_command.py
@@ -0,0 +1,18 @@
+
+import argparse
+import logging
+
+import command
+
+logger = logging.getLogger(__name__)
+
+class VersionCommand(command.Command):
+
+    def __init__(self, stations, settings, player, subparsers):
+        super().__init__(stations, settings, player)
+        parser = subparsers.add_parser('version')
+        parser.set_defaults(func=self.execute)
+
+    def execute(self, args):
+        # TODO How to save the current version?
+        logger.info("cora - Version 0.1.0")
